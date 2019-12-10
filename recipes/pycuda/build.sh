06774805d27a4123d7d295aff49a6a6ba885fb19
benhuff <benhuff10@gmail.com>
2019-12-10 11:23:16 -0500

    export cuda home to path

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-export PATH=${CUDA_HOME}/bin:${PATH}
-ln -s ${CUDA_HOME}/include/* ${CONDA_PREFIX}/include/
-
-./configure.py
-python setup.py build
-python setup.py install
+/etc/aksetup-defaults.py
+$HOME/.aksetup-defaults.py
+$PACKAGEDIR/siteconf.py
