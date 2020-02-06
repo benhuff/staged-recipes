4b9a58ab5974b4f5cfe0218a92744ece3deda575
benhuff <benhuff10@gmail.com>
2020-02-06 14:49:09 -0500

    precompiled binaries

--- recipes/cockroachdb/build.sh
+++ recipes/cockroachdb/build.sh
@@ -2,17 +2,6 @@
 
 set -ex
 
-echo $PWD
-ls
-mkdir build
-cp $SRC_DIR/cockroachdb build
-cd build
-ls
-
-export CGO_ENABLED=1
-make build
-make install
-
 mkdir -p $PREFIX/bin
 
-mv out/cockroach $PREFIX/bin
+mv $SRC_DIR/cockroach $PREFIX/bin
