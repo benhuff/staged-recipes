e1fd0bd6bd63b2d029293e465696620c953ab84c
benhuff <benhuff10@gmail.com>
2020-02-11 11:46:39 -0500

    build cockroachdb from source

--- recipes/cockroachdb/build.sh
+++ recipes/cockroachdb/build.sh
@@ -2,11 +2,7 @@
 
 set -evx
 
-# CMAKE_PREFIX_PATH=$PREFIX:$BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/usr
-
-ln -s -t "${CMAKE_PREFIX_PATH}/include/stdlib.h" /home/conda/staged-recipes/build_artifacts/cockroachdb_1581438316139/work/src/github.com/cockroachdb/cockroach/c-deps/jemalloc/src/jemalloc.c
-
-make build
+make build --with-jemalloc-prefix=je_
 make install
 
 mkdir -p $PREFIX/bin
