9262002c3dd13c262241c896affa9da2290a70f7
benhuff <benhuff10@gmail.com>
2020-02-11 11:12:57 -0500

    build cockroachdb from source

--- recipes/cockroachdb/build.sh
+++ recipes/cockroachdb/build.sh
@@ -2,7 +2,11 @@
 
 set -evx
 
-make build -release
+# CMAKE_PREFIX_PATH=$PREFIX:$BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/usr
+
+ln -s -t "${CMAKE_PREFIX_PATH}/" $SRC_DIR/src/github.com/cockroachdb/cockroach/c-deps/
+
+make build
 make install
 
 mkdir -p $PREFIX/bin
