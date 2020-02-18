474464ce7082a0cfa266d55b39c00cc46669ee2b
benhuff <benhuff10@gmail.com>
2020-02-18 15:26:11 -0500

    cgo

--- recipes/cockroachdb/build.sh
+++ recipes/cockroachdb/build.sh
@@ -2,13 +2,6 @@
 
 set -evx
 
-cd ${SRC_DIR}
-ls
-
-# echo ${SRC_DIR}/src/github.com/cockroachdb/cockroach/c-deps/jemalloc/
-
-ln -s "$SRC_DIR/src/github.com/cockroachdb/cockroach" "$GOPATH/src/github.com/cockroachdb/cockroach"
-
 make build
 make install
 
