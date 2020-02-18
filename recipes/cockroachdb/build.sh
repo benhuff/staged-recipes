85f4a3562bb8db21a45b8e0fc0fc8ab6ac1b7dd8
benhuff <benhuff10@gmail.com>
2020-02-18 15:07:31 -0500

    cd src_dir, ls, update link

--- recipes/cockroachdb/build.sh
+++ recipes/cockroachdb/build.sh
@@ -2,10 +2,12 @@
 
 set -evx
 
-# echo ${SRC_DIR}
+cd ${SRC_DIR}
+ls
+
 # echo ${SRC_DIR}/src/github.com/cockroachdb/cockroach/c-deps/jemalloc/
 
-ln -s "$SRC_DIR" "$GOPATH/src/github.com/cockroachdb/cockroach"
+ln -s "$SRC_DIR/src/github.com/cockroachdb/cockroach" "$GOPATH/src/github.com/cockroachdb/cockroach"
 
 make build
 make install
